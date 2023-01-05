d7095d6a155ce407350e086d4e81589fe7035b8c
shweta <shwetajs200@gmail.com>
2023-01-05 17:00:48 +0530

    initial setup

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div className="home">
+        <button className="store-btn">store</button>
+        <button className="show-btn">show</button>
+    </div>
+    </>
+  )
+}
+
+export default Home
